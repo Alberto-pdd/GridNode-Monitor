566146108d50d393bc7cb1fdbf0f3362fd5efa04
alberto-pdd <232228549+Alberto-pdd@users.noreply.github.com>
2026-02-12 09:35:01 +0100

    feat: initial setup of GridNode Monitor V0 (sequential base)
    Estructura inicial del sistema de red inteligente:
    - Implementación de modelos de datos (Consumo y Demanda)[cite: 34, 37].
    - Configuración de la jerarquía RedEnergetica y ZonaEnergetica[cite: 58, 61].
    - Lógica base de suministro en Bateria y registro en CuentaEnergetica[cite: 62, 66].
    - Procesamiento secuencial mediante CentroControl según parámetros de Config[cite: 71, 79, 85].

---
+++ src/main/java/main/MySmartGrid.java
@@ -0,0 +1,29 @@
+package main;
+
+import energy.Consumo;
+import energy.RedEnergetica;
+import java.util.List;
+
+public class MySmartGrid {
+	
+    public static void main(String[] args) {
+        RedEnergetica red = new RedEnergetica(
+                Config.NUM_ZONAS,
+                Config.CAPACIDAD_BATERIA,
+                Config.NIVEL_INICIAL_BATERIA
+        );
+        
+        List<Consumo> consumos = Consumo.consumosDesdeFichero(Config.FICHERO_CONSUMOS);
+        System.out.println("Leidos " + consumos.size() + " consumos desde " + Config.FICHERO_CONSUMOS);
+      
+        // Tramitamos los consumos de manera secuencial
+        String resultado;
+        for (Consumo c:consumos) {
+        	resultado = red.getZona(c.getZona()).tramitarConsumo(c); // Quitar linea, usar hilos como el ejemplo Bancos
+            red.getZona(c.getZona()).getVentana().traza (c.getIdConsumo()+ " - Tramitado: "+resultado); // Quitar linea, usar hilos como el ejemplo Bancos
+        }
+        red.imprimeAuditoria();
+    }
+
+
+}
